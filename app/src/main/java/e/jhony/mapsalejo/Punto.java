ba3571ae6b73144894d9bb0f22e1efddb439686c
jhony.JHONY <jhonym698@gmail.com>
2018-08-31 06:41:13 -0500

    Initial commit

---
+++ app/src/main/java/e/jhony/mapsalejo/Punto.java
@@ -0,0 +1,40 @@
+package e.jhony.mapsalejo;
+
+public class Punto {
+    private double latitudInicial;
+    private double longitudInicial;
+    private double latitudFinal;
+    private double longitudFinal;
+
+    public double getLatitudInicial() {
+        return latitudInicial;
+    }
+
+    public void setLatitudInicial(double latitudInicial) {
+        this.latitudInicial = latitudInicial;
+    }
+
+    public double getLongitudInicial() {
+        return longitudInicial;
+    }
+
+    public void setLongitudInicial(double longitudInicial) {
+        this.longitudInicial = longitudInicial;
+    }
+
+    public double getLatitudFinal() {
+        return latitudFinal;
+    }
+
+    public void setLatitudFinal(double latitudFinal) {
+        this.latitudFinal = latitudFinal;
+    }
+
+    public double getLongitudFinal() {
+        return longitudFinal;
+    }
+
+    public void setLongitudFinal(double longitudFinal) {
+        this.longitudFinal = longitudFinal;
+    }
+}
